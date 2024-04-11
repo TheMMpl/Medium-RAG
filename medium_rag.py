068f14c1f2726b729341f37c455baa1c6e56de87
Mikołaj Hasik <mikoajhasik@gmail.com>
2024-04-11 20:52:31 +0200

    parent chunking

--- medium_rag.py
+++ medium_rag.py
@@ -13,16 +13,17 @@ if __name__ == "__main__":
     parser.add_argument("--mode", help="mode - local or external model", type=str)
     parser.add_argument("--query", help="query to be answered", type=str)
     parser.add_argument("--display_db_results", help="view results passed to the llm", type=str)
+    parser.add_argument("--question_type", help="specific or general, by default general", type=str)
     args = parser.parse_args()
 
     
-    db=VectorDatabase(args.mode)
+    db=VectorDatabase(args.mode,question_type=args.question_type)
     if args.mode=="local":
         llm=Ollama(model="llama2")
     else:
         llm = ChatOpenAI()
     
-    prompt = ChatPromptTemplate.from_template("""You are an assistant hepling with explaining machine learning techniques. Answer the following question based only on the provided context:
+    prompt = ChatPromptTemplate.from_template("""You are an assistant hepling with explaining machine learning techniques. Answer the following question based on the provided context:
 
     <context>
     {context}
@@ -32,12 +33,13 @@ if __name__ == "__main__":
 
 
     document_chain = create_stuff_documents_chain(llm, prompt)
-    retriever = db.vector.as_retriever()
-    retrieval_chain = create_retrieval_chain(retriever, document_chain)
+    retrieval_chain = create_retrieval_chain(db.retriever, document_chain)
     response = retrieval_chain.invoke({"input": args.query})
     print(response["answer"])
 
     if args.display_db_results=="True":
-        print(db.similarity_search_with_score(args.query))
+        print(db.vector.similarity_search_with_score(args.query))
+        print("\n")
+        print(db.retriever.get_relevant_documents(args.query))
 
 
--- vector_database.py
+++ vector_database.py
@@ -1,6 +1,8 @@
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.vectorstores import FAISS
+from langchain.storage import InMemoryStore
+from langchain.retrievers import ParentDocumentRetriever
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders.csv_loader import CSVLoader
 import os.path
@@ -8,18 +10,37 @@ import os.path
 
 
 class VectorDatabase:
-    def __init__(self,mode):
+    def __init__(self,mode,question_type):
+
+        if question_type=="specific":
+            self.chunk_size=200
+            self.db_path="db_index_specific"
+        else:
+            self.chunk_size=1000
+            self.db_path="db_index"
+
+        self.splitter= RecursiveCharacterTextSplitter(chunk_size= self.chunk_size,chunk_overlap= 200,separators=['\nclass ', '\ndef ', '\n\tdef ',"\n\n", "\n", " ", ""])
+        self.parent_splitter= RecursiveCharacterTextSplitter(chunk_size= self.chunk_size*8,chunk_overlap= 200,separators=['\nclass ', '\ndef ', '\n\tdef ',"\n\n", "\n", " ", ""])
+
         if mode=="local":
-            self.embeddings=OllamaEmbeddings()
-            self.splitter= RecursiveCharacterTextSplitter()
+            self.embeddings=OllamaEmbeddings()  
         else:
             self.embeddings= OpenAIEmbeddings()
-            self.splitter= RecursiveCharacterTextSplitter()
+        
+        self.docs=self.prepare_data()
 
-        if os.path.exists("faiss_index"):
+        if os.path.exists(self.db_path):
             self.load_db()
         else:
             self.create_db()
+        
+        self.store=InMemoryStore()
+        self.retriever = ParentDocumentRetriever(
+        vectorstore=self.vector,
+        docstore=self.store,
+        child_splitter=self.splitter,
+        parent_splitter=self.parent_splitter,)
+        self.retriever.add_documents(self.docs)
     
     def prepare_data(self):
         loader = CSVLoader(
@@ -33,10 +54,11 @@ class VectorDatabase:
         return loader.load()
     
     def create_db(self):
-        docs=self.prepare_data()
-        documents = self.splitter.split_documents(docs[1:])
+        documents = self.splitter.split_documents(self.docs[1:])
         self.vector = FAISS.from_documents(documents, self.embeddings)
-        self.vector.save_local("faiss_index")
+        self.vector.save_local(self.db_path)
+  
     
     def load_db(self):
-        self.vector=FAISS.load_local("faiss_index", self.embeddings,allow_dangerous_deserialization=True)
+        self.vector=FAISS.load_local(self.db_path,self.embeddings,allow_dangerous_deserialization=True)
+ 
